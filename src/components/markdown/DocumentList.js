15a29cf30b4a2a3431d3a97aa50b3866dae71157
emily <mle4d@me.com>
2019-08-29 10:05:04 -0700

    added tab

---
+++ src/components/markdown/DocumentList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function DocumentList() {
+  return (
+    <>
+      <ul>
+        <li>tab1</li>;
+        <li>tab2</li>;
+        <li>tab3</li>;
+        <li>tab4</li>;
+      </ul>
+    </>
+  );
+}
+
+// DocumentList.propTypes = {
+//   documents: PropTypes.array.isRequired
+// };
+
+export default DocumentList;
