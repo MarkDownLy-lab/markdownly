a1440a55933fc032892885e7aac1fd4fe5294115
Erin Gurley <erinmgurley@gmail.com>
2019-08-28 18:56:02 -0700

    fixed styles

--- src/components/markdown/Document.js
+++ src/components/markdown/Document.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import EditorInput from '../../containers/EditorInput';
 import PreviewDisplay from '../../containers/PreviewDisplay';
+import styles from './Document.css';
+
 
 export default function Document() {
   return (
     <>
+    <div className={styles.Document}>
       <EditorInput />
       <PreviewDisplay />
+    </div>
     </>
   );
 }
