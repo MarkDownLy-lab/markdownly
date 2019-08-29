bcb447d9c2e6d5f50dbac09af35a2bd1403fbf7f
emily <mle4d@me.com>
2019-08-29 15:41:47 -0700

    markdown to document

--- src/components/markdown/Preview.js
+++ src/components/markdown/Preview.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import marked from 'marked';
 import styles from 'github-markdown-css/github-markdown.css';
 
-function Preview({ markdown }) {
-  const __html = marked(markdown);
+function Preview({ document }) {
+  const __html = marked(document.text);
   return <div className={styles['markdown-body']} dangerouslySetInnerHTML={{ __html }}></div>;
 }
 
 Preview.propTypes = {
-  markdown: PropTypes.string.isRequired
+  document: PropTypes.object.isRequired
 };
 
 export default Preview;
