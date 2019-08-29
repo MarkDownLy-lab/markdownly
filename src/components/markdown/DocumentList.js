b5d0b894e05f910417690a462a27bb3e4fe0e2a8
emily <mle4d@me.com>
2019-08-29 15:04:47 -0700

    added buttons to li

--- src/components/markdown/DocumentList.js
+++ src/components/markdown/DocumentList.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function DocumentList({ documents }) {
-  // const documents = ['tab1', 'tab2'];
-  const tabList = documents.map(document => (
-    <li key={document}>
-      {document}</li>
+  const tabList = documents.map((document, i) => (
+    <li key={i}>
+      <button>
+        {document.text}
+      </button>
+    </li>
   ));
 
+  console.log('docs', documents);
   return (
   <>
     <ul>{tabList}</ul>
