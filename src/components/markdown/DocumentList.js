549833322e43519b308401f5795d4fa1c27e58ae
emily <mle4d@me.com>
2019-08-29 09:57:03 -0700

    created list component

---
+++ src/components/markdown/DocumentList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function DocumentList() {
+  return (
+    <>
+      <ul>
+        <li>tab1</li>;
+        <li>tab2</li>;
+        <li>tab3</li>;
+      </ul>
+    </>
+  );
+}
+
+// DocumentList.propTypes = {
+//   documents: PropTypes.array.isRequired
+// };
+
+export default DocumentList;
