dba65f235743fe49b99dfccafd73e385e64d3912
emily <mle4d@me.com>
2019-08-29 16:02:48 -0700

    deleted dispatch function form doclist container

--- src/containers/DocList.js
+++ src/containers/DocList.js
@@ -1,20 +1,11 @@
 import { connect } from 'react-redux';
 import DocumentList from '../components/markdown/DocumentList';
 import { getDocuments } from '../selectors/markdownSelectors';
-import { addDocument } from '../actions/markdownActions';
-
 
 const mapStateToProps = state => ({
   documents: getDocuments(state)
 });
 
-const mapDispatchToProps = dispatch => ({
-  changeDocument({ target }) {
-    dispatch(addDocument(target.value));
-  }
-});
-
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(DocumentList);
